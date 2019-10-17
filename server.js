4438c00895807ece2872c3a0a3a73c42e2d8c99c
Daniel Poda <dpoda@worlplaynetworks.com>
2019-10-17 13:57:12 -0600

    step 2 - minimal server

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("hi")
+const Hapi = require('@hapi/hapi');
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 7000;
+
+const server = Hapi.server({
+  host: HOST,
+  port: PORT,
+})
+
+server.start();
+
+console.info(`Server started at ${server.info.uri}`);
